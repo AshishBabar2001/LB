687869edfd3bc114f62f8943f4fe434050e8edf1
Ashish Babar <90142699+AshishBabar2001@users.noreply.github.com>
2022-11-19 21:15:01 +0530

    Add files via upload

---
+++ Program11.c
@@ -0,0 +1,45 @@
+//Check Wheather Number Is Divisible By Three And Five.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+///////////////////////////////////////////////////////////////////////////////////
+//  Fuction Name =      DivisiobalByThreeAndFive
+//  Description =       This fuction is show divisible by 3 and 5
+//  input =         Integer
+//  Output =        Boolean
+//  Author =        Ashish Babar
+///////////////////////////////////////////////////////////////////////////////////
+
+bool DivisibalByThreeAndFive(int iNo)
+{
+    if((iNo % 3 == 0)&&(iNo % 5 == 0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    bRet = DivisibalByThreeAndFive(iValue);
+    if(bRet == true)
+    {
+        printf("Number Is Divisible By 3 And 5 \n");
+    }
+    else
+    {
+        printf("Number Is Divisible By 3 or 5 \n");
+    }
+
+    return 0;
+}
